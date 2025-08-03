2f870d73c42951ec1031c863585936e8c5fbdec5
mishra-khushboo <khush27093@gmail.com>
2025-08-03 12:07:49 +0530

    update UI

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerHTML = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        }
+        if (select.name === "to" && currCode === "INR") {
+          newOption.selected = "selected";
+        }
+    }
+    select.addEventListener("change",(evt)=> {
+        updateFlag(evt.target);
+    })
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
